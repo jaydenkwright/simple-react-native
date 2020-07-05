cfe586a97b861c00fe63b9319c728fdb2abe1470
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-04 22:20:13 -0400

    Fixed search screen

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import SearchBar from '../Components/SearchBar'
+import useResults from '../hooks/useResults'
+import Results from '../Components/Results'
 
 const SearchScreen = () => {
     const [search, setSearch] = useState('')
+    const [searchApi, results, error] = useResults()
+
+    const filterResults = price => {
+        return results.filter(result => {
+            return result.price === price
+        })
+    }
 
     return (
-        <View>
+        <View style={{ flex: 1}}> 
             <SearchBar 
             search={search} 
             setSearch={setSearch} 
-            onSubmit={() => console.log(search)}/>
-            <Text>{search}</Text>
+            onSubmit={() => searchApi(search)}/>
+            {error ? <Text>{error}</Text> : null}
+            <ScrollView>
+                <Results title='Cost Effective' results={filterResults('$')} />
+                <Results title='Bit Pricier' results={filterResults('$$')}/>
+                <Results title='Big Spender' results={filterResults('$$$')}/>
+            </ScrollView>
         </View>
     )
 }
